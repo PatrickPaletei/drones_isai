0df78dc4f807aa0a2b85c10b2cce7760e679d3b5
PatrickPaletei <patrick.pranaduta@ti.ukdw.ac.id>
2023-05-04 20:14:04 +0700

    tambah top nav

--- app/src/main/java/id/ac/ukdw/drones_isai/KalkulatorFragment.kt
+++ app/src/main/java/id/ac/ukdw/drones_isai/KalkulatorFragment.kt
@@ -1,31 +1,39 @@
 package id.ac.ukdw.drones_isai
 
 import android.os.Bundle
+import android.view.*
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [KalkulatorFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class KalkulatorFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (requireActivity() as AppCompatActivity).supportActionBar?.apply {
+            title = "Kalkulator Karbon"
+        }
+        val iconImageView = view.findViewById<ImageView>(R.id.icon_image_view)
+        iconImageView.setOnClickListener {
+            // Handle click on the custom icon here
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.top_nav_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_icon -> {
+                // Handle click on the custom icon here
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
         }
     }
 
@@ -34,26 +42,12 @@ class KalkulatorFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_kalkulator, container, false)
+        val view = inflater.inflate(R.layout.fragment_kalkulator, container, false)
+
+        setHasOptionsMenu(true) // Add this line
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment KalkulatorFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            KalkulatorFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
