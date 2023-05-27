825de89955041d1760867b30d2e201c4c3ab0efb
PatrickPaletei <patrick.pranaduta@ti.ukdw.ac.id>
2023-05-27 08:28:24 +0700

    filter done

--- app/src/main/java/id/ac/ukdw/drones_isai/EmisiLandFragment.kt
+++ app/src/main/java/id/ac/ukdw/drones_isai/EmisiLandFragment.kt
@@ -2,12 +2,11 @@ package id.ac.ukdw.drones_isai
 
 import android.content.pm.ActivityInfo
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import id.ac.ukdw.drones_isai.databinding.FragmentEmisiLandBinding
-import id.ac.ukdw.drones_isai.databinding.FragmentTerserapLandBinding
 
 
 class EmisiLandFragment : Fragment() {
@@ -18,7 +17,7 @@ class EmisiLandFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentEmisiLandBinding.inflate(inflater, container, false)
         val view = binding.root
--- app/src/main/java/id/ac/ukdw/drones_isai/TrendFragment.kt
+++ app/src/main/java/id/ac/ukdw/drones_isai/TrendFragment.kt
@@ -1,28 +1,45 @@
 package id.ac.ukdw.drones_isai
 
-import android.content.Context
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.Toolbar
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import com.github.mikephil.charting.charts.BarChart
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import id.ac.ukdw.adapter.SpinnerFilterAdapter
 import id.ac.ukdw.adapter.ViewPagerAdapter
+import id.ac.ukdw.drones_isai.databinding.FilterBottomPopupBinding
 import id.ac.ukdw.drones_isai.databinding.FragmentTrenBinding
 
 
+data class SpinnerItem(val id: Int, val name: String)
 class TrendFragment : Fragment() {
 
 
     private lateinit var binding: FragmentTrenBinding
 
+    private val spinner1Items = listOf(
+        SpinnerItem(1, "2020"),
+        SpinnerItem(2, "2021"),
+        SpinnerItem(3, "2022")
+    )
+    private val spinner2Items = listOf(
+        SpinnerItem(1, "2"),
+        SpinnerItem(2, "3"),
+        SpinnerItem(3, "4")
+    )
+    private val spinner3Items = arrayOf("Padi", "Cabe", "Coklat")
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentTrenBinding.inflate(inflater, container, false)
         val view = binding.root
 
@@ -37,12 +54,40 @@ class TrendFragment : Fragment() {
         // Set up the TabLayout
         binding.tabLayout.setupWithViewPager(binding.viewPager)
 
-
+        // Set up the button click listener
+        binding.filterBtn.setOnClickListener {
+            showBottomPopupDialog()
+        }
 
         return view
     }
 
 
+
+    private fun showBottomPopupDialog() {
+        val bottomSheetDialog = BottomSheetDialog(requireContext())
+        val dialogBinding = FilterBottomPopupBinding.inflate(LayoutInflater.from(requireContext()))
+        bottomSheetDialog.setContentView(dialogBinding.root)
+
+        val spinner1 = dialogBinding.spinnerTahun
+        val spinner2 = dialogBinding.spinnerDataKarbon
+        val spinner3 = dialogBinding.spinnerJenisKomoditas
+
+        val spinner1Adapter = SpinnerFilterAdapter(requireContext(), android.R.layout.simple_spinner_item, spinner1Items)
+        spinner1Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner1.adapter = spinner1Adapter
+
+        val spinner2Adapter = SpinnerFilterAdapter(requireContext(), android.R.layout.simple_spinner_item, spinner2Items)
+        spinner2Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner2.adapter = spinner2Adapter
+
+        val spinner3Adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, spinner3Items)
+        spinner3Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner3.adapter = spinner3Adapter
+
+        // Show the bottom sheet dialog
+        bottomSheetDialog.show()
+    }
 
 
 }
