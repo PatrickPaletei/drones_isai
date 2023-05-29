60cd221f4c55bae5f8d83a70f52a3e3930e8fafd
PatrickPaletei <patrick.pranaduta@ti.ukdw.ac.id>
2023-05-29 11:38:22 +0700

    test api

---
+++ app/src/main/java/id/ac/ukdw/data/apiHelper/ApiClient.kt
@@ -0,0 +1,32 @@
+package id.ac.ukdw.data.apiHelper
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+    //base url api
+    const val BASE_URL = "https://sk695s0owe.execute-api.us-east-1.amazonaws.com/"
+
+    private val logging: HttpLoggingInterceptor
+        get() {
+            val httpLoggingInterceptor = HttpLoggingInterceptor()
+            return httpLoggingInterceptor.apply {
+                httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            }
+        }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+    val instance: ApiService by lazy {
+        val retrofit =
+            Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
+                .client(
+                    client
+                ).build()
+        retrofit.create(ApiService::class.java)
+    }
+
+}
